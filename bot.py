b21d2660a4a2455162c2a3b0e6650fe5b5975658
Andrey <you@example.com>
2020-09-07 21:08:11 +0300

    v3

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from PIL import Image
 from urllib.request import urlopen
 import xlrd
 # поменял
-
+# поменял 2
 y = yadisk.YaDisk(token="AgAAAAAFCrD9AAaFsnHBigAYx0Vyg5V-BjRKiZs")
 
 excel_path_names = './names_bot.xlsx'
